a7d9cb8aa5be23bb3114cf8b7ff12e1045c9e893
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-06-21 01:20:09 +0300

    Update test.py
    Copy tests from main-branch

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import os
+import io
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
@@ -9,20 +9,27 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 from tensorflow.keras.models import Sequential, load_model
 
+
 print("Hello this linter work!")
 
+
+def preprocess_image(img):
+    img = img.resize((224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
 
 
 def test_civilian_image():
     # Загрузка и преобразование изображения не оружия
-    img_path = '/content/train/noweapon/File 1710.jpg'
-    img = image.load_img(img_path, target_size=(224, 224))
-
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
+    img_path = '/train/noweapon/File 1710.jpg'
+    img = image.load_img(img_path)
+    x = preprocess_image(img)
+    
     
     # Загрузка модели
-    model = load_model("drive/MyDrive/Models/ml_engineering_weapon_and_no")
+    model = load_model("/Models/ml_engineering_weapon_and_no")
         
     pred = model.predict(x)
     assert pred < 0.2
@@ -32,13 +39,11 @@ def test_civilian_image():
  def test_weapon_image():
     # Загрузка и преобразование изображения оружия
     img_path = '/content/train/weapon/File 1016.jpg'
-    img = image.load_img(img_path, target_size=(224, 224))
-
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
+    img = image.load_img(img_path)
+    x = preprocess_image(img)
     
     # Загрузка модели
-    #model = load_model("drive/MyDrive/Models/ml_engineering_weapon_and_no")
+    model = load_model("/Models/ml_engineering_weapon_and_no")
         
     pred = model.predict(x)
     assert pred > 0.9
