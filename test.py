6ede4cc9c21cfd0b45e0bd44e04e985142966772
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-06-20 17:35:14 +0300

    Update test.py
    Основные намётки тестов

--- test.py
+++ test.py
@@ -1,1 +1,44 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow.keras import utils
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+from tensorflow.keras.models import Sequential, load_model
+
 print("Hello this linter work!")
+
+
+
+def test_civilian_image():
+    # Загрузка и преобразование изображения не оружия
+    img_path = '/content/train/noweapon/File 1710.jpg'
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    
+    # Загрузка модели
+    model = load_model("drive/MyDrive/Models/ml_engineering_weapon_and_no")
+        
+    pred = model.predict(x)
+    assert pred < 0.2
+
+
+
+ def test_weapon_image():
+    # Загрузка и преобразование изображения оружия
+    img_path = '/content/train/weapon/File 1016.jpg'
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    
+    # Загрузка модели
+    #model = load_model("drive/MyDrive/Models/ml_engineering_weapon_and_no")
+        
+    pred = model.predict(x)
+    assert pred > 0.9
