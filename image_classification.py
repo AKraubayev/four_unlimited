fe4b3db03b734085bb9663fe717365ed468d276d
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-06-20 17:19:39 +0300

    Update image_classification.py
    Add keras.load_model library

--- image_classification.py
+++ image_classification.py
@@ -41,9 +41,14 @@ from tensorflow.keras import utils
 from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing import image_dataset_from_directory
+from tensorflow.keras.models import Sequential, load_model
 import matplotlib.pyplot as plt
 from google.colab import files
-# %matplotlib inline
+%matplotlib inline 
+
+"""###Подключим Google Drive"""
+
+
 
 """## Загружаем данные"""
 
