ca15154eaf8aeb16965bbcbb66d04d22ee26af8a
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-06-26 03:50:34 +0300

    Update image_classification.py
    Add import preprocess_input, decode_predictions  from tensorflow.keras.applications.efficientnet

--- image_classification.py
+++ image_classification.py
@@ -31,6 +31,7 @@ from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 import matplotlib.pyplot as plt
 
 """## Загружаем датасеты """
