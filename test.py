c7e61a32f703984433b57cc0f728aec3dc3cf49a
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-06-26 04:09:19 +0300

    Update test.py

--- test.py
+++ test.py
@@ -17,12 +17,8 @@ def preprocess_image(img):
     x = preprocess_input(x)
     return x
 
-def preprocess_image(img):
-    img = img.resize((224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    return x
+
+model = load_model("/Models/ml_engineering_weapon_and_no/")
 
 
 def test_civilian_image():
@@ -30,25 +26,15 @@ def test_civilian_image():
     img_path = '/train/noweapon/File 1710.jpg'
     img = image.load_img(img_path)
     x = preprocess_image(img)
-    
-    
-    # Загрузка модели
-model = load_model("/Models/ml_engineering_weapon_and_no/")
-        
-pred = model.predict(x)
+    pred = model.predict(x)
     assert pred < 0.2
 
 
-
- def test_weapon_image():
+def test_weapon_image():
     # Загрузка и преобразование изображения оружия
     img_path = '/train/weapon/File 1016.jpg'
     img = image.load_img(img_path)
     x = preprocess_image(img)
-    
-    # Загрузка модели
-model = load_model("/Models/ml_engineering_weapon_and_no/")
-        
-pred = model.predict(x)
+    pred = model.predict(x)
     assert pred > 0.9
 
