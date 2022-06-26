6abfe2a396c623841d0793ff90a64ea8d0dffaf4
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-06-26 03:18:13 +0300

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -88,6 +88,7 @@ test_dataset = image_dataset_from_directory('test',
 
 """##Загружаем модель и проверяем качество обучения на тестовом наборе данных"""
 
+unzip /Models/ml_engineering_weapon_and_no.zip -d /Models/
 model = load_model("/Models/ml_engineering_weapon_and_no")
 
 
