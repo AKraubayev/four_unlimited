fa02cb9ae1a63f6d1e34634fe5264819e5292838
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-06-26 03:48:36 +0300

    Update test.py
    Add import preprocess_input, decode_predictions from tensorflow.keras.applications.efficientnet

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 
 
 def preprocess_image(img):
