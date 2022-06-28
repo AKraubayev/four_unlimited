efb0336825f15cd19742b6e4f87f05ce8c7683ac
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-06-29 02:30:32 +0300

    Update test_image_classification.py

--- test_image_classification.py
+++ test_image_classification.py
@@ -1,12 +1,10 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers
 from tensorflow.keras import utils
-from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing import image_dataset_from_directory
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.efficientnet import preprocess_input
 
 
 def preprocess_image(img):
