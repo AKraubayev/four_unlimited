a19a1cdb210afde2d5cafeaa9406a4bf67af1e3e
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-06-29 00:07:27 +0300

    Update and rename test.py to test_image_classification.py

--- test_image_classification.py
+++ test_image_classification.py
@@ -1,6 +1,3 @@
-import urllib.request
-import zipfile
-import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
@@ -18,15 +15,6 @@ def preprocess_image(img):
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     return x
-
-
-url = 'https://www.dropbox.com/s/ni9567tj2x2r5b6/ml_engineering_weapon_and_no.zip'
-urllib.request.urlretrieve(url, 'ml_engineering_weapon_and_no.zip')
-
-
-os.mkdir('Models/', mode=777)
-with zipfile.ZipFile('ml_engineering_weapon_and_no.zip', 'w') as zip_ref:
-    zip_ref.extractall('Models/ml_engineering_weapon_and_no')
 
 
 def load_trained_model():
