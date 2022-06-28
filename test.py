862c16102e46934a7b9cecfe0f18c09c78ae4ad3
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-06-28 21:49:15 +0300

    Update test.py
    Refactored test

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-import io
+import urllib.request
+import zipfile
+import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
@@ -18,8 +20,17 @@ def preprocess_image(img):
     return x
 
 
+url = 'https://www.dropbox.com/s/ni9567tj2x2r5b6/ml_engineering_weapon_and_no.zip'
+urllib.request.urlretrieve(url, 'ml_engineering_weapon_and_no.zip')
+
+
+os.mkdir('Models/', mode=777)
+with zipfile.ZipFile('ml_engineering_weapon_and_no.zip', 'w') as zip_ref:
+    zip_ref.extractall('Models/ml_engineering_weapon_and_no')
+
+
 def load_trained_model():
-    model = load_model("/content/Models/ml_engineering_weapon_and_no")
+    model = load_model("Models/ml_engineering_weapon_and_no")
     return model
 
 
@@ -28,7 +39,7 @@ model = load_trained_model()
 
 def test_civilian_image():
     # Загрузка и преобразование изображения не оружия
-    img_path = '/train/noweapon/File 1710.jpg'
+    img_path = './train/noweapon/File 1710.jpg'
     img = image.load_img(img_path)
     x = preprocess_image(img)
     pred = model.predict(x)
@@ -37,7 +48,7 @@ def test_civilian_image():
 
 def test_weapon_image():
     # Загрузка и преобразование изображения оружия
-    img_path = '/train/weapon/File 1016.jpg'
+    img_path = './train/weapon/File 1016.jpg'
     img = image.load_img(img_path)
     x = preprocess_image(img)
     pred = model.predict(x)
