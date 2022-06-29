b7e6f70982404cf8a506d8e2eb247dc5762b4d86
Ilia <54379506+Ilia-sign@users.noreply.github.com>
2022-06-29 16:59:40 +0300

    update "   "

--- image_classification.py
+++ image_classification.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """ "ml_engineering_weapon_and_no.ipynb""
 
-Original file is located at
+Original file is located at  
     https://colab.research.google.com/drive/145c7vHHGECzQp_X4vGuCBD0UIS_XmlIH
 ## Задание с хакатона осеннего семестра первого курса магистратуры "Инженерия машинного обучения":
 # No/Weapons: набор изображений, содержащих и не содержащих оружие #
